3b20ad316164d1181b58bb1d1c3daab4340d2353
Jason McVetta <jason.mcvetta@gmail.com>
2026-01-30 00:26:00 +0700

    trailing newline

--- lib/config.sh
+++ lib/config.sh
@@ -390,3 +390,4 @@ export -f get_profile_core get_profile_build_tools get_profile_shell get_profile
 export -f get_profile_rust get_profile_python get_profile_go get_profile_flutter get_profile_javascript get_profile_java get_profile_ruby
 export -f get_profile_php get_profile_database get_profile_devops get_profile_web get_profile_embedded get_profile_datascience
 export -f get_profile_security get_profile_ml get_profile_homebrew
+
